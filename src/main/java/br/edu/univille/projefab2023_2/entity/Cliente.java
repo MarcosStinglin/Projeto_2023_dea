204525a649ab3be81379e6480651bf1ea6ce75ab
Cassol <carlos.cassol@univille.br>
2023-10-09 19:17:15 -0300

    Alterado para modelo uml

--- src/main/java/br/edu/univille/projefab2023_2/entity/Cliente.java
+++ src/main/java/br/edu/univille/projefab2023_2/entity/Cliente.java
@@ -15,44 +15,13 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
-    private String telefone;
-    private String endereco;
-    private String email;
-    @Temporal(TemporalType.DATE)
-    private Date nascimento;
+    private double valorOrcamento;
 
-
-    public Date getNascimento() {
-        return nascimento;
-    }
-    public void setNascimento(Date nascimento) {
-        this.nascimento = nascimento;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
     public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-    
-    public String getEndereco() {
-        return endereco;
-    }
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
     }
     public long getId() {
         return id;
@@ -60,4 +29,11 @@ public class Cliente {
     public void setId(long id) {
         this.id = id;
     }
+    public double getValorOrcamento() {
+        return valorOrcamento;
+    }
+    public void setValorOrcamento(double valorOrcamento) {
+        this.valorOrcamento = valorOrcamento;
+    }
+    
 }
